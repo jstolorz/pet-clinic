ea8806590c9774ecbdf9c97f70fe872ae9a07ada
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-28 12:37:44 +0200

    added properties to Owner closes #29

--- pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Owner.java
@@ -3,13 +3,42 @@ package com.bluesoft.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person{
+
+    private String address;
+    private String city;
+    private String telephone;
+
     private Set<Pet> pets;
 
-    public Set<Pet> getPets() {
+   public String getAddress() {
+        return address;
+    }
+
+   public void setAddress(final String address) {
+        this.address = address;
+    }
+
+   public String getCity() {
+        return city;
+    }
+
+   public void setCity(final String city) {
+        this.city = city;
+    }
+
+   public String getTelephone() {
+        return telephone;
+    }
+
+   public void setTelephone(final String telephone) {
+        this.telephone = telephone;
+    }
+
+   public Set<Pet> getPets() {
         return pets;
     }
 
-    public void setPets(final Set<Pet> pets) {
+   public void setPets(final Set<Pet> pets) {
         this.pets = pets;
     }
 }
