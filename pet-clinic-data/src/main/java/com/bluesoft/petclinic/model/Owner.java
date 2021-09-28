01b842e6443b5c15ae097620c6e32ee0bd856a83
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-28 12:12:01 +0200

    added visits, update owner with pet relation, closes #27

--- pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Owner.java
@@ -1,4 +1,15 @@
 package com.bluesoft.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(final Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Visit.java
@@ -0,0 +1,33 @@
+package com.bluesoft.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+   public LocalDate getDate() {
+        return date;
+    }
+
+   public void setDate(final LocalDate date) {
+        this.date = date;
+    }
+
+   public String getDescription() {
+        return description;
+    }
+
+   public void setDescription(final String description) {
+        this.description = description;
+    }
+
+   public Pet getPet() {
+        return pet;
+    }
+
+   public void setPet(final Pet pet) {
+        this.pet = pet;
+    }
+}
