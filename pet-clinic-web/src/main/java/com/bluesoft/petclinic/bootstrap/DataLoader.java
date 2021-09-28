f283c9a7054d00ba96f236e684685f5b5fde9088
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-28 13:49:33 +0200

    added PetType to bootstrap closes #33

--- pet-clinic-web/src/main/java/com/bluesoft/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/bluesoft/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.bluesoft.petclinic.bootstrap;
 
 import com.bluesoft.petclinic.model.Owner;
+import com.bluesoft.petclinic.model.PetType;
 import com.bluesoft.petclinic.model.Vet;
 import com.bluesoft.petclinic.services.OwnerService;
+import com.bluesoft.petclinic.services.PetTypeService;
 import com.bluesoft.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(final OwnerService ownerService, final VetService vetService) {
+    public DataLoader(final OwnerService ownerService, final VetService vetService, final PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(final String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Zoja");
+        final PetType savedPetType = petTypeService.save(dog);
+
+        PetType dog2 = new PetType();
+        dog.setName("Tymek");
+        final PetType savedPetType2 = petTypeService.save(dog2);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Janusz");
