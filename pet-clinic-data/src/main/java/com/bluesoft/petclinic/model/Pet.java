fac9420b73e2cc88cc00dca3dc45b993e96ca818
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-30 15:21:55 +0200

    Update Visit with JPA annotation, closes #39

--- pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.bluesoft.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +56,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(final LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    Set<Visit> getVisits() {
+        return visits;
+    }
+
+    void setVisits(final Set<Visit> visits) {
+        this.visits = visits;
     }
 }
