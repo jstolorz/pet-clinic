49af6510425b829c13aa68d7bbf825029ab7952c
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-30 14:40:15 +0200

    Update Owner, Pet, PetType  with JPA annotation, closes #37

--- pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/bluesoft/petclinic/model/Person.java
@@ -1,7 +1,14 @@
 package com.bluesoft.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 class Person extends BaseEntity{
+
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
