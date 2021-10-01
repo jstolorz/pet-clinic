7795d5066d02f3e8409d2d32c0904de562098f31
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-10-01 16:28:00 +0200

    implement Map Visit Service , closes #47

---
+++ pet-clinic-data/src/main/java/com/bluesoft/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.bluesoft.petclinic.services;
+
+import com.bluesoft.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/bluesoft/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package com.bluesoft.petclinic.services.map;
+
+import com.bluesoft.petclinic.model.Visit;
+import com.bluesoft.petclinic.repositories.VisitRepository;
+import com.bluesoft.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(final Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(final Visit object) {
+        if(object.getPet() == null
+                || object.getPet().getOwner() == null
+                || object.getPet().getId() == null
+                || object.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visist");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(final Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(final Long aLong) {
+        super.deleteById(aLong);
+    }
+}
